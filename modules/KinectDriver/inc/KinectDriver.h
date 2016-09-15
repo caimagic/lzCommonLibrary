f34031e9e41b94f4c0facdcdd9aa298534a0d94e
caimagic <caizhijie_hit@163.com>
2016-09-16 02:28:35 +0800

    delete core and add g3log in 3rdparty

--- modules/KinectDriver/inc/KinectDriver.h
+++ modules/KinectDriver/inc/KinectDriver.h
@@ -1,17 +1,55 @@
+ /**
+ *
+ * Copyright (C), 2016 Lang Qi (shanghai) Internet Tech. Co., Ltd.
+ *
+ * \brief          KinectDriver.h c++ file
+ * \file         : KinectDriver.h
+ * \version      : ALPHA
+ * \author       : caimagic
+ * \date         : 2016/9/16 1:21:47
+ * \copyright    : Lang Qi (shanghai) Internet Tech. Co., Ltd.
+ * \warning      : 
+ *
+ * Email         : caizhijie_hit@163.com
+ * ClassList     :
+ * History       : change history log, including date, author and brief content statement.
+ *
+ */
+ 
 #ifndef __LZ_KINECT_DRIVER_H__
 #define __LZ_KINECT_DRIVER_H__
 
+/********************************************************************************************/
+/*			1 Other Header File Including													*/
+/********************************************************************************************/
 #include "Version.h"
 #include "lzTypes.h"
 #include "Kinect.h"
 #include "ICapture.h"
 
+/********************************************************************************************/
+/*			2 Macro Definition																*/
+/********************************************************************************************/
 #define KINECT_DRIVER_DEPTH_WIDTH	512
 #define KINECT_DRIVER_DEPTH_HEIGHT	424
 #define KINECT_DRIVER_COLOR_WIDTH	1920
 #define KINECT_DRIVER_COLOR_HEIGHT	1080
 
-typedef enum 
+/********************************************************************************************/
+/*			3 Enum Type Definition															*/
+/********************************************************************************************/
+
+
+/********************************************************************************************/
+/*			4 Global Variable Declaring														*/
+/********************************************************************************************/
+
+
+/********************************************************************************************/
+/*			5 Struct Type Definition														*/
+/********************************************************************************************/
+
+typedef enum
 {
 	NONE,
 	PointCloud,
@@ -25,7 +63,7 @@ typedef struct
 	lzInt32 depthHeight;
 	lzInt32 colorWidth;
 	lzInt32 colorHeight;
-	Kinect_Driver_Mesh_Model  bMeshModel;
+	Kinect_Driver_Mesh_Model  enMeshModel;
 } Kinect_Driver_Para_Type;
 
 typedef struct
@@ -61,14 +99,45 @@ typedef struct
 } Kinect_Driver_Mesh_Type;
 
 
+/********************************************************************************************/
+/*			6 Class Type Definition															*/
+/********************************************************************************************/
 class CKinectDriver
 {
 public:
 	CKinectDriver();
 	~CKinectDriver();
-	ICapture *pCapture;
+
+	lzBool OpenSensor();
+	lzBool UpdateFrame();
+	lzBool SetParameter(IN Kinect_Driver_Para_Type* para);
+	lzBool GetParameter(OUT Kinect_Driver_Para_Type* para);
+
+	ICapture* pCapture;
+
+	lzRGBX* pColor;
+	lzInt32* pDepth;
+	lzInt32 colorHeight;
+	lzInt32 colorWidth;
+	lzInt32 depthHeight;
+	lzInt32 depthWidth;
+	Kinect_Driver_Mesh_Model mode;
+
+private:
+
 };
-	
+
+/********************************************************************************************/
+/*			7 Others Definition																*/
+/********************************************************************************************/
+
+
+/********************************************************************************************/
+/*			8 Function Declare																*/
+/********************************************************************************************/
+
+LZ_EXPORTS_API lzBool lzKinectDriverOpenLog(lzBool isopen = false);
+
 // open kinect 
 LZ_EXPORTS_API lzBool lzKinectDriverOpenSensor(void);
 
@@ -98,7 +167,7 @@ LZ_EXPORTS_API lzBool lzKinectDriverAcquireFrame(OUT Kinect_Driver_Frame_Type* f
 
 // 
 LZ_EXPORTS_API lzBool lzKinectDriverMapDepthFrameToCameraSpace(
-	IN lzInt32 depthPointCount, 
+	IN lzInt32 depthPointCount,
 	IN Kinect_Driver_Depth_Frame_Type* depthFrame,
 	IN lzInt32 cameraPointCount,
 	OUT CameraSpacePoint* cameraSpacePoints);
@@ -130,5 +199,5 @@ LZ_EXPORTS_API lzBool lzKinectDriverAcquireMeshCount(OUT lzInt32* count);
 //
 LZ_EXPORTS_API lzBool lzKinectDriverAcquireModel(OUT Kinect_Driver_Mesh_Type* mesh);
 
-#endif
 
+#endif // __LZ_KINECT_DRIVER_H__ 
--- modules/KinectDriver/src/KinectDriver.cpp
+++ modules/KinectDriver/src/KinectDriver.cpp
@@ -2,39 +2,162 @@
 //
 #include <memory>
 #include "KinectDriver.h"
+#include "kinectCapture.h"
+#include "g3log/g3log.hpp"
+#include "g3log/logworker.hpp"
+#include <io.h>
+#include "direct.h"
+
+std::unique_ptr<CKinectDriver> kinectDriver;
+std::unique_ptr<g3::LogWorker> worker;
+std::unique_ptr<g3::FileSinkHandle> handle;
+
+#if (defined(WIN32) || defined(_WIN32) || defined(__WIN32__))
+const std::string path_to_log_file = ".//log//";
+#else
+const std::string path_to_log_file = "/tmp/";
+#endif
 
 CKinectDriver::CKinectDriver()
 {
-	
+	pColor = nullptr;
+	pDepth = nullptr;
+	pCapture = new KinectCapture();
+
+	// initialize frame parameter 
+	colorHeight	= 0;
+	colorWidth	= 0;
+	depthHeight = 0;
+	depthWidth	= 0;
+	mode = Kinect_Driver_Mesh_Model::NONE;
 }
 
 CKinectDriver::~CKinectDriver()
 {
+	if (pCapture != nullptr)
+	{
+		delete pCapture;
+	}
 
+	if (pColor != nullptr)
+	{
+		delete pColor;
+	}
+
+	if (pDepth != nullptr)
+	{
+		delete pDepth;
+	}
+}
+
+lzBool CKinectDriver::OpenSensor()
+{
+	lzBool ret = true;
+	ret = pCapture->Initialize();
+	if (ret)
+	{
+		colorHeight = pCapture->nColorFrameHeight;
+		colorWidth = pCapture->nColorFrameWidth;
+		depthHeight = pCapture->nDepthFrameHeight;
+		depthWidth = pCapture->nDepthFrameWidth;
+	}
+
+	return ret;
 }
 
 
-std::unique_ptr<CKinectDriver> kinectDriver;
+lzBool CKinectDriver::UpdateFrame()
+{
+	lzBool ret = true;
+	ret = pCapture->AcquireFrame();
+	return ret;
+}
 
+
+lzBool CKinectDriver::SetParameter(IN Kinect_Driver_Para_Type* para)
+{
+	colorHeight	= para->colorHeight;
+	colorWidth	= para->colorWidth;
+	depthHeight = para->depthHeight;
+	depthWidth	= para->depthWidth;
+	mode		= para->enMeshModel;
+
+	return true;
+}
+
+
+lzBool CKinectDriver::GetParameter(OUT Kinect_Driver_Para_Type* para)
+{
+	para->colorHeight	= colorHeight;
+	para->colorWidth	= colorWidth;
+	para->depthHeight	= depthHeight;
+	para->depthWidth	= depthWidth;
+	para->enMeshModel	= mode;
+
+	return true;
+}
+
+
+
+LZ_EXPORTS_API lzBool lzKinectDriverOpenLog(lzBool isopen)
+{
+	if (_access(path_to_log_file.c_str(), 0) == -1)
+	{
+		_mkdir(path_to_log_file.c_str());
+	}
+
+	if (isopen)
+	{	
+		g3::only_change_at_initialization::setLogLevel(WARNING, true);
+		g3::only_change_at_initialization::setLogLevel(INFO, true);
+		g3::only_change_at_initialization::setLogLevel(DEBUG, true);
+	}
+	else
+	{
+		g3::only_change_at_initialization::setLogLevel(WARNING, false);
+		g3::only_change_at_initialization::setLogLevel(INFO, false);
+		g3::only_change_at_initialization::setLogLevel(DEBUG, false);
+	}
+
+	std::string log_prefix = "KinectDriver";
+	worker = g3::LogWorker::createLogWorker();
+	handle = worker->addDefaultLogger(log_prefix, path_to_log_file);
+	g3::initializeLogging(worker.get());
+
+	return true;
+}
 
 // open kinect 
 LZ_EXPORTS_API lzBool lzKinectDriverOpenSensor(void)
 {
-	kinectDriver = std::unique_ptr<CKinectDriver>(new CKinectDriver());
+	int ret = true;
 
-	return true;
+	kinectDriver = std::unique_ptr<CKinectDriver>(new CKinectDriver());
+	
+	ret = kinectDriver->OpenSensor();
+	if (!ret)
+	{
+		LOGF(WARNING, " open kinect fail.");
+	}
+
+	return ret;
 }
 
 // close kinect
 LZ_EXPORTS_API lzBool lzKinectDriverCloseSensor(void)
 {
 	kinectDriver = nullptr;
+	worker = nullptr;
+	handle = nullptr;
 	return true;
 }
 
 // 
 LZ_EXPORTS_API lzBool lzKinectDriverSetParameter(IN Kinect_Driver_Para_Type* para)
 {
+
+
+
 
 	return true;
 }
@@ -49,13 +172,40 @@ LZ_EXPORTS_API lzBool lzKinectDriverGetParameter(OUT Kinect_Driver_Para_Type* pa
 //
 LZ_EXPORTS_API lzBool lzKInectDriverUpdateFrame()
 {
-
-	return true;
+	lzBool ret = true;
+	ret = kinectDriver->UpdateFrame();
+	return ret;
 }
 
 //
 LZ_EXPORTS_API lzBool lzKInectDriverAcquireColorFrame(OUT Kinect_Driver_Color_Frame_Type* colorFrame)
 {
+	if ((kinectDriver->colorHeight != KINECT_DRIVER_COLOR_HEIGHT) 
+		|| (kinectDriver->colorWidth != KINECT_DRIVER_COLOR_WIDTH))
+	{
+		return false;
+	}
+
+	kinectDriver->pColor = new lzRGBX[kinectDriver->colorHeight * kinectDriver->colorWidth];
+
+	lzInt32 index = 0;
+	for (lzInt32 i = 0; i < kinectDriver->colorHeight; i++)
+	{
+		for (lzInt32 j = 0; j < kinectDriver->colorWidth; j++)
+		{
+			// careful, there efficiency maybe low
+			kinectDriver->pColor[index].r = kinectDriver->pCapture->pColorRGBX[index].r;
+			kinectDriver->pColor[index].g = kinectDriver->pCapture->pColorRGBX[index].g;
+			kinectDriver->pColor[index].b = kinectDriver->pCapture->pColorRGBX[index].b;
+			kinectDriver->pColor[index].x = kinectDriver->pCapture->pColorRGBX[index].x;
+
+			colorFrame->frame[index].r = kinectDriver->pColor[index].r;
+			colorFrame->frame[index].g = kinectDriver->pColor[index].g;
+			colorFrame->frame[index].b = kinectDriver->pColor[index].b;
+
+			index++;
+		}
+	}
 
 	return true;
 }
@@ -63,6 +213,26 @@ LZ_EXPORTS_API lzBool lzKInectDriverAcquireColorFrame(OUT Kinect_Driver_Color_Fr
 //
 LZ_EXPORTS_API lzBool lzKInectDriverAcquireDepthFrame(OUT Kinect_Driver_Depth_Frame_Type* depthFrame)
 {
+	if ((kinectDriver->depthHeight != KINECT_DRIVER_DEPTH_HEIGHT)
+		|| (kinectDriver->depthWidth != KINECT_DRIVER_DEPTH_WIDTH))
+	{
+		return false;
+	}
+
+	kinectDriver->pDepth = new lzInt32[kinectDriver->depthHeight * kinectDriver->depthWidth];
+
+	lzInt32 index = 0;
+	for (lzInt32 i = 0; i < kinectDriver->depthHeight; i++)
+	{
+		for (lzInt32 j = 0; j < kinectDriver->depthWidth; j++)
+		{
+			// careful, there efficiency maybe low
+			kinectDriver->pDepth[index] = (lzInt32)kinectDriver->pCapture->pDepth[index];
+			depthFrame->frame[index] = kinectDriver->pDepth[index];
+
+			index++;
+		}
+	}
 
 	return true;
 }
